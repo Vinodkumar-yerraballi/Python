bf05c3002c85e7871e083c1b6789dde2ded6548b
Vinodkumar-yerraballi <vinodkumar9676150@gmail.com>
2022-11-05 11:00:50 +0530

    do my excersise

---
+++ app.py
@@ -0,0 +1,82 @@
+# input_valu=12
+# for i in range(0,input_valu):
+#     if input_valu%2==0:
+#         input_valu=input_valu/2
+#     else:
+#         input_valu=input_valu-1
+#     if input_valu==0:
+#         break
+# print(i+1)
+
+
+# num=3
+# print(f"the num {num}")
+
+# x={1,2,3,4,5}
+# x.add(5)
+# x.add(6)
+# print(x)
+
+# y=[x*2 for x in range(1,10)]
+# print(y)
+# import math
+
+# x=18
+# if x>10:
+#     if x>15:
+#         print('A')
+#     else:
+#         print('B')
+# else:
+#     print('c')
+
+# for i in range(5):
+#     print(i)
+# else:
+#     print("done")
+
+# {x:[y for y in range(1,x) if x%y==0] for x in range(2,100)}
+# n=2
+# [x*2 for x in range(1,n)]
+# a=[(x,x+1) for x in range(1,5)]
+# print(a)
+
+# x=1j
+# print(x**2==-1)
+def listmissingnumber(n):
+    number=set(n)
+    length=len(n)
+    output=[]
+    for i in range(1,n[-1]):
+        if i not in number:
+             output.append(i)
+    return output
+listnumber=[1,2,3,5,9]
+print(listmissingnumber(listnumber))
+
+color=["black","red","green"]
+code=["##4","##7"]
+color_1=["color_name"]
+result=dict(zip(color,code))
+print(result)
+a=[1,3,5,8]
+b=[10,2,5]
+c=a+b
+print(c)
+
+lst=["a","b","c","d","e","f","g","h","1","2","4"]
+s = []
+n = 3
+for x in range(n):
+    s.append(lst[x::n])
+    print(s)
+
+man=["vinod"]
+can=["let' break the rule"]
+win=[]
+for man in range(1,2):
+    if win.append(man):
+        print(win)
+name=input("Enter your name")
+for i in range(1,10):
+       print(name)
