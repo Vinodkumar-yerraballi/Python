bb30f373c9b2d5feedece2fa7f40e14758e4cb63
Vinodkumar-yerraballi <vinodkumar9676150@gmail.com>
2022-12-02 12:43:46 +0530

    fush my update files

--- 1.py
+++ 1.py
@@ -64,5 +64,22 @@ print(expenses)
 # based on this
 expenses[3]=expenses[3]-200
 print(expenses)
-
-
+# You have a list of your favourite marvel super heros.
+heros=['spider man','thor','hulk','iron man','captain america']
+# 1. Length of the list
+print(len(heros))
+# 2. Add 'black panther' at the end of this list
+heros.append('black panther')
+print(heros)
+# 3. You realize that you need to add 'black panther' after 'hulk',
+#    so remove it from the list first and then add it after 'hulk'
+heros.remove('black panther')
+heros.insert(3,"black panther")
+print(heros)
+# 4. Now you don't like thor and hulk because they get angry easily :)
+#    So you want to remove thor and hulk from list and replace them with doctor strange (because he is cool).
+#    Do that with one line of code.
+heros[1:3]=['doctor stranges']
+# 5. Sort the list in alphabetical order
+heros.sort()
+print(heros)
