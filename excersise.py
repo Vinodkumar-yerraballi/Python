0747f4a53aefd7d726df787af442e8adec5d91bc
Vinodkumar-yerraballi <vinodkumar9676150@gmail.com>
2022-12-08 14:38:47 +0530

    update my files

--- excersise.py
+++ excersise.py
@@ -16,11 +16,38 @@ def binary_search(numbers_list, number_to_find):
             right_index = mid_index - 1
 
     return -1
+def find_all_accurance(number_list,number_to_find):
+    index=binary_search(numbers_list, number_to_find)
+    indices = [index]
+    # find indices on left hand side
+    i = index-1
+    while i >=0:
+        if number_list[i] == number_to_find:
+            indices.append(i)
+        else:
+            break
+        i = i - 1
 
+    # find indices on right hand side
+    i = index + 1
+    while i<len(number_list):
+        if number_list[i] == number_to_find:
+            indices.append(i)
+        else:
+            break
+        i = i + 1
+
+    return sorted(indices)
+    
 
 if __name__=="__main__":
-    numbers_list=[1,4,6,9,10,5,7]
-    number_to_find=5
-    index=binary_search(numbers_list, number_to_find)
-    print(f'the bineary search {index}')
+    # numbers_list=[1,4,6,9,10,5,7]
+    # number_to_find=5
+    # index=binary_search(numbers_list, number_to_find)
+    # print(f'the bineary search {index}')
+    numbers_list = [1,4,6,9,11,15,15,15,17,21,34,34,56]
+    number_to_find = 1
+    indices = find_all_accurance(numbers_list, number_to_find)
+    print(f"Indices of occurances of {number_to_find} are {indices}")
+
 
