e6ff0263607660ff766a498e5978ed0959a34853
Vinod kumar Yerrabalali <Vinodkumar9676150@gmail.com>
2023-11-24 22:38:16 +0530

    add files

---
+++ Basics/Functions/functions1.py
@@ -0,0 +1,15 @@
+# How to create a function 
+# function is block of code that will work on the specifi task
+
+def greet():
+    print("Hello")
+
+name=input()
+greet()
+print(name) # Output Hello vinod
+
+def greeting(name):
+    message = 'Hello ' + name
+    print(message)
+inp_put=input()
+print(greeting(inp_put)) # 
---
+++ Basics/string methods/Question1.py
@@ -0,0 +1,7 @@
+split=input().split() # input [10, 20, 30, 40,]
+result=[]
+for number in split:
+    number= int(number)
+    result +=[number]
+    
+print(result)  # out put [10, 20, 30, 40, 50]
