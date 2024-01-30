098037d776dabf1de603d1ea70ee55ad19d56507
Vinod kumar Yerrabalali <Vinodkumar9676150@gmail.com>
2024-01-30 22:08:40 +0530

    add files

---
+++ Roughwork/rough_1.py
@@ -0,0 +1,22 @@
+from array import *
+
+# arr=array('i',[])
+# x=int(input())
+# for i in range(x):
+#     n=int(input())
+#     arr.append(n)
+# print(arr)
+
+
+# create 
+lets={'first_name':'vindo', 'last_name':'Yerraballi'}
+print(lets.keys())
+print(lets.values())
+# Sets means is without any element which meas no keys in the curly brackets
+input_name=set('hello')
+s1={1,2,3,4}
+s2={1,1,2,2,3,4}
+print(s1)
+print(type(s1))
+print(s2)
+print(type(lets))
