35479e8022646bd69266e69e9ccecdfac750b33c
Vinod kumar Yerrabalali <Vinodkumar9676150@gmail.com>
2024-12-22 21:26:22 +0530

    add files

---
+++ Basics/string methods/Next_wave.py
@@ -0,0 +1,12 @@
+# Valid traingle
+ # user input
+a=int(input())
+b=int(input())
+c=int(input())
+
+if (a+b+c)==180:
+    print("*")
+    print("*"*2)
+    print("*"3)
+else:
+    print("Not a valid traingle")
